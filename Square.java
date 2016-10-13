49bd186d967bed2da5dc266b79b5350d6d3aeb6b
kanestapler <kanestapler@gmail.com>
2016-10-13 10:01:26 -0500

    Making Square class

---
+++ Square.java
@@ -0,0 +1,20 @@
+public class Sqaure {
+	
+	public int xPos;
+	public int yPos;
+	private char value;
+	
+	public Square(int x, int y) {
+		this.xPos = x;
+		this.yPos = y;
+	}
+	
+	public void SetValue(char x) {
+		this.value = x;
+	}
+	
+	public char GetValue() {
+		return this.value;
+	}
+	
+}
